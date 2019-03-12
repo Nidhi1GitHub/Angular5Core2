e855cc94e6af83aeec04bca886130ba570bd2a29
Nidhi <developer.dhavalp@gmail.com>
2019-03-12 17:25:46 +0530

    2nd Commit

---
+++ EFNgApp/Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFNgApp.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+
+    }
+}
