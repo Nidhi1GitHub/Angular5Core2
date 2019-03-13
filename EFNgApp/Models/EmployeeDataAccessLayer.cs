c38ea930a10dbf08dc4bf756000d23581399ed55
Nidhi <developer.dhavalp@gmail.com>
2019-03-13 09:28:05 +0530

    Service Generated

--- EFNgApp/Models/EmployeeDataAccessLayer.cs
+++ EFNgApp/Models/EmployeeDataAccessLayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,77 @@ namespace EFNgApp.Models
 {
     public class EmployeeDataAccessLayer
     {
+        Angular5_core2Context db = new Angular5_core2Context();
+        public IEnumerable <TblEmployee> GetAllEmployee()
+        {
+            try
+            {
+                return db.TblEmployee.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
 
+        public int AddEmployee(TblEmployee employee)
+        {
+            try
+            {
+                db.TblEmployee.Add(employee);
+                db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public int UpdateEmployee(TblEmployee employee)
+        {
+            try
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public TblEmployee GetEmployeeData(int id)
+        {
+            try
+            {
+                TblEmployee employee = db.TblEmployee.Find(id);
+                return employee;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public int DeleteEmployee(int id)
+        {
+            try
+            {
+                TblEmployee employee = db.TblEmployee.Find(id);
+                db.TblEmployee.Remove(employee);
+                db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }            
+        }
+        public List<TblCities> GetCities()
+        {
+            List<TblCities> lstCity = new List<TblCities>();
+            lstCity = (from CityList in db.TblCities select CityList).ToList();
+            return lstCity;
+        }
     }
 }
